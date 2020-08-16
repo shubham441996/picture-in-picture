91f39cf53bbe80160614321eb2ad3166e997fb90
Shubham Paliwal <shubham.paliwal@getfareye.com>
2020-08-16 15:01:29 +0530

    Picture in picture completed

---
+++ README.md
@@ -0,0 +1,19 @@
+# Picture In Picture
+
+## Tech Stack Used
+
+1. HTML
+2. Css
+3. Javascript
+
+## Api Used
+
+`navigator.mediaDevices.getDisplayMedia(); await videoEle.requestPictureInPicture();`
+
+## Special Thanks
+
+[Andrei Neagoie](https://twitter.com/andreineagoie?lang=en)
+
+New Things Learn
+
+- How to use Picture in Picture using chrome built Apis
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+let videoEle = document.getElementById("video-container");
+let btnEle = document.getElementById("play-button");
+
+async function setUpPictureInPicture() {
+  try {
+    const mediaElement = await navigator.mediaDevices.getDisplayMedia();
+    videoEle.srcObject = mediaElement;
+    videoEle.onloadedmetadata = () => {
+      videoEle.play();
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+btnEle.addEventListener("click", async () => {
+  btnEle.disabled = true;
+  await videoEle.requestPictureInPicture();
+  btnEle.disabled = false;
+});
+setUpPictureInPicture();
